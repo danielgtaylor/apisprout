2d1afd0382a389adb4c1e86de2edb12489f2fdfb
pruser <pruser@users.noreply.github.com>
2018-10-21 15:15:00 +0200

    getTypedExample modified to use schema examples

--- apisprout_test.go
+++ apisprout_test.go
@@ -8,10 +8,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_GetTypedExampleFromExampleField(t *testing.T) {
+func Test_GetTypedExampleShouldGetFromExampleField(t *testing.T) {
 	exampleData := map[string]string{"name1": "value1", "name2": "value2"}
+
 	mediaType := openapi3.NewMediaType()
 	mediaType.WithExample("example1", exampleData)
+
 	selectedExample, err := getTypedExample(mediaType)
 	assert.Nil(t, err)
 
@@ -19,4 +21,73 @@ func Test_GetTypedExampleFromExampleField(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotEmpty(t, string(selectedExampleJSON))
 	assert.Equal(t, `{"name1":"value1","name2":"value2"}`, string(selectedExampleJSON))
+}
+
+func Test_GetTypedExampleShouldGetFromSchemaExampleField(t *testing.T) {
+	exampleData := map[string]string{"name1": "value1", "name2": "value2"}
+
+	schema := openapi3.NewSchema()
+	schema.Example = exampleData
+
+	mediaType := openapi3.NewMediaType()
+	mediaType.WithSchema(schema)
+
+	selectedExample, err := getTypedExample(mediaType)
+	assert.Nil(t, err)
+
+	selectedExampleJSON, err := json.Marshal(selectedExample)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, string(selectedExampleJSON))
+	assert.Equal(t, `{"name1":"value1","name2":"value2"}`, string(selectedExampleJSON))
+}
+
+func Test_GetTypedExampleShouldGetFromSchemaPropertiesExampleField(t *testing.T) {
+	schema := openapi3.NewSchema()
+
+	parameterSchema1 := openapi3.NewStringSchema()
+	parameterSchema1.Example = "testvalue"
+
+	parameterSchema2 := openapi3.NewObjectSchema()
+	nestedParameterSchema := openapi3.NewBoolSchema()
+	nestedParameterSchema.Example = true
+	parameterSchema2.WithProperty("nestedProperty", nestedParameterSchema)
+
+	schema.WithProperties(map[string]*openapi3.Schema{
+		"name1": parameterSchema1,
+		"name2": parameterSchema2,
+	})
+
+	mediaType := openapi3.NewMediaType()
+	mediaType.WithSchema(schema)
+
+	selectedExample, err := getTypedExample(mediaType)
+	assert.Nil(t, err)
+
+	selectedExampleJSON, err := json.Marshal(selectedExample)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, string(selectedExampleJSON))
+	assert.Equal(t, `{"name1":"testvalue","name2":{"nestedProperty":true}}`, string(selectedExampleJSON))
+}
+
+func Test_GetTypedExampleShouldReturnErrorIfCannotGetFullExample(t *testing.T) {
+	schema := openapi3.NewSchema()
+
+	parameterSchema1 := openapi3.NewStringSchema()
+	parameterSchema1.Example = "testvalue"
+
+	parameterSchema2 := openapi3.NewObjectSchema()
+	nestedParameterSchemaWithoutExample := openapi3.NewBoolSchema()
+	parameterSchema2.WithProperty("nestedProperty", nestedParameterSchemaWithoutExample)
+
+	schema.WithProperties(map[string]*openapi3.Schema{
+		"name1": parameterSchema1,
+		"name2": parameterSchema2,
+	})
+
+	mediaType := openapi3.NewMediaType()
+	mediaType.WithSchema(schema)
+
+	selectedExample, err := getTypedExample(mediaType)
+	assert.NotNil(t, err)
+	assert.Nil(t, selectedExample)
 }
